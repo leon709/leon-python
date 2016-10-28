f3b234a7d1be1b7cb9de2b02667982e4e7b1f77a
leon <lsu@movoto.com>
2016-10-28 13:09:43 +0800

    one line code

--- one_line_code.py
+++ one_line_code.py
@@ -1,2 +1,4 @@
 # one line code, pring heart
 print '\n'.join([''.join([('ILoveYou'[(x-y)%8] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30,30)]) for y in range(15, -15, -1)])
+
+print '\n'.join([' '.join(['%s*%s=%-2s' % (y, x, x*y) for y in range(1, x+1)]) for x in range(1, 10)])
