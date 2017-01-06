a0891f11649000651255e35d06ad6869c76bfcc8
leon <lsu@movoto.com>
2017-01-06 19:03:16 +0800

    add web tools

---
+++ tools.py
@@ -0,0 +1,34 @@
+import sys, json
+import flask
+from flask import make_response, render_template
+import traceback
+
+app = flask.Flask(__name__)
+
+import logging
+logger = logging.getLogger("tools")
+
+@app.route("/")
+def hello():
+    print app.jinja_loader.searchpath
+    
+    return "Welcome!"
+
+@app.route("/test/<page>")
+def test(page):
+    
+    pp = "/home/leon/mycodebase/leon-python"
+    
+    app.jinja_loader.searchpath.append(pp)
+    
+    print app.jinja_loader.searchpath
+    
+    return render_template(page + ".html")
+    
+if __name__ == "__main__":
+    import os, time
+    mt = os.stat(sys.argv[0]).st_mtime
+    logger.info("Code update time: %s", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(mt)))
+    
+    app.run(debug=True, host='0.0.0.0')
+    logger.info("Webtool started...")
